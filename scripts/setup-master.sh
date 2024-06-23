97f158d905d5ccfa86c253a7569dcf2b13ac698e
mdsolarflare <45709370+mdsolarflare@users.noreply.github.com>
2024-06-23 08:36:18 +0000

    update scripts

--- scripts/setup-master.sh
+++ scripts/setup-master.sh
@@ -18,11 +18,15 @@ curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --token $1 --
 #dietpi.node3   Ready    <none>                 110s   v1.29.5+k3s1
 #dietpi.node2   Ready    <none>                 14m    v1.29.5+k3s1
 #dietpi.node4   Ready    <none>                 93s    v1.29.5+k3s1
-#
-#kubectl label nodes dietpi.node1 kubernetes.io/role=worker
-#kubectl label nodes dietpi.node2 kubernetes.io/role=worker
-#kubectl label nodes dietpi.node3 kubernetes.io/role=worker
-#kubectl label nodes dietpi.node4 kubernetes.io/role=worker
+
+# BASH SCRIPTS FTW :^)
+node_names=$(kubectl get nodes -o custom-columns=NAME:.metadata.name | awk '{print $1}' | tail -n +2)
+for node in $node_names; do
+  # Replace 'my-command' with your actual command
+  echo "kubectl label nodes <resource name from above> kubernetes.io/role=worker"
+  kubectl label nodes $node kubernetes.io/role=worker
+  # Add additional commands to operate on each node here (optional)
+done
 
 # Installing helm per https://helm.sh/docs/intro/install/
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
@@ -40,43 +44,15 @@ helm version
 #ark --help  # a handy alias
 
 # MetalLB - https://metallb.universe.tf/
-# Add MetalLB repository to Helm
-# TODO I think just install this way next time, if not the old method is here
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
 # to delete 
 # kubectl get clusterroles,clusterrolebindings -n metallb-system
 # then delete the specific ones keeping the pods up
 # kubectl delete deployments,services,pods -n metallb-system --all
-#helm repo add metallb https://metallb.github.io/metallb
-
-# Check the added repository
-#helm search repo metallb
-#"metallb" has been added to your repositories
-#NAME            CHART VERSION   APP VERSION     DESCRIPTION
-#metallb/metallb 0.14.5          v0.14.5         A network load-balancer implementation for Kube...
-
-# I found that helm does not know what to do if this isn't set
-#sudo chmod 600 /etc/rancher/k3s/k3s.yaml - this just breaks it so no do this
-# helm doesn't work without this, but do i need helm? can i just use manifests?
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-# error looks like ->
-#$ helm upgrade --install metallb metallb/metallb --create-namespace --namespace metallb-system --wait
-#  Error: Kubernetes cluster unreachable: Get "http://localhost:8080/version": dial tcp [::1]:8080: connect: connection refused
-#helm upgrade --install metallb metallb/metallb --create-namespace --namespace metallb-system --wait
-# success looks like ->
-#Release "metallb" does not exist. Installing it now.
-#NAME: metallb
-#LAST DEPLOYED: Sun Jun 23 07:49:05 2024
-#NAMESPACE: metallb-system
-#STATUS: deployed
-#REVISION: 1
-#TEST SUITE: None
-#NOTES:
-#MetalLB is now running in the cluster.
 
 # apply the configuration manifest, you need to change the IPs
 kubectl apply -f ./k3s-yamls/metallb-default-pool.yaml
 
-# create namespace for argoCD
+# install argoCD - https://argoproj.github.io/cd/
 kubectl create namespace argocd  
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
