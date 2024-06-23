45b99899914efa654d5241d613d83a2719afbec9
mdsolarflare <45709370+mdsolarflare@users.noreply.github.com>
2024-06-23 05:53:09 +0000

    update scripts

--- scripts/setup-master.sh
+++ scripts/setup-master.sh
@@ -7,7 +7,8 @@
 # Post DietPi setup, pre-k3s
 sed -i '1s/$/cgroup_memory=1 cgroup_enable=memory/' /boot/cmdline.txt
 
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --token $1 --node-ip $2
+# Installing k3s master per https://docs.k3s.io/quick-start
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --token $1 --node-ip $2 --disable servicelb
 
 #NAME           STATUS   ROLES                  AGE    VERSION
 #dietpi.node1   Ready    control-plane,master   24m    v1.29.5+k3s1
@@ -19,3 +20,19 @@ curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --token $1 --
 #kubectl label nodes dietpi.node2 kubernetes.io/role=worker
 #kubectl label nodes dietpi.node3 kubernetes.io/role=worker
 #kubectl label nodes dietpi.node4 kubernetes.io/role=worker
+
+# Installing helm per https://helm.sh/docs/intro/install/
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# print version to make sure it installed ok?
+helm version
+
+# Arkade? - https://github.com/alexellis/arkade#getting-arkade
+# Note: you can also run without `sudo` and move the binary yourself
+#curl -sLS https://get.arkade.dev | sudo sh
+#
+#arkade --help
+#ark --help  # a handy alias
+
--- scripts/setup-worker.sh
+++ scripts/setup-worker.sh
@@ -7,6 +7,12 @@
 # Post DietPi setup, pre-k3s
 sed -i '1s/$/cgroup_memory=1 cgroup_enable=memory/' /boot/cmdline.txt
 
+# Installing k3s worker per https://docs.k3s.io/quick-start
 curl -sfL https://get.k3s.io | K3S_URL="https://$2:6443" K3S_TOKEN=$1 sh -
 
-
+# Arkade? - https://github.com/alexellis/arkade#getting-arkade
+# Note: you can also run without `sudo` and move the binary yourself
+#curl -sLS https://get.arkade.dev | sudo sh
+#
+#arkade --help
+#ark --help  # a handy alias
