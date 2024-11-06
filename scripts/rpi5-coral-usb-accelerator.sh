c663aef6117b4c68bba4093e4b8757dcb53fa784
mdsolarflare <45709370+mdsolarflare@users.noreply.github.com>
2024-11-05 18:05:33 -0800

    Update rpi5-coral-usb-accelerator.sh

--- scripts/rpi5-coral-usb-accelerator.sh
+++ scripts/rpi5-coral-usb-accelerator.sh
@@ -1,1 +1,25 @@
 # Setup for coral usb accelerator on rpi5
+
+# Install edge tpu runtime
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install libedgetpu1-std
+
+# optional max freq (chip gets hot)
+#sudo apt-get install libedgetpu1-max
+
+
+
+
+
+
+
+
+
+
+
+
