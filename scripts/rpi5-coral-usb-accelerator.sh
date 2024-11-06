dd5f8b9c6d840b4521c3e7d188f4878464522aef
mdsolarflare <45709370+mdsolarflare@users.noreply.github.com>
2024-11-05 18:22:14 -0800

    Update rpi5-coral-usb-accelerator.sh

--- scripts/rpi5-coral-usb-accelerator.sh
+++ scripts/rpi5-coral-usb-accelerator.sh
@@ -12,9 +12,19 @@ sudo apt-get install libedgetpu1-std
 # optional max freq (chip gets hot)
 #sudo apt-get install libedgetpu1-max
 
+# Install pycoral - via sudo apt-get install python3-pycoral
+# failed for python 3.11 not supported, pycoral only supports 3.6-3.9
+# Note: PyCoral currently supports Python 3.6 through 3.9. 
+# If your default version is something else, we suggest you 
+# install Python 3.9 with pyenv. (https://realpython.com/intro-to-pyenv/)
 
+# install dependencies for pyenv
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl # had python-openssl but that's python 2?
 
-
+# install pyenv via pyenv-installer project (https://github.com/pyenv/pyenv-installer)
+curl https://pyenv.run | bash
 
 
 
