2de9401fae6aacdac6e0f01bb5017a61c8dabe08
mdsolarflare <45709370+mdsolarflare@users.noreply.github.com>
2025-01-14 21:50:58 -0800

    re-order script

--- scripts/setup-dietpi-worker.sh
+++ scripts/setup-dietpi-worker.sh
@@ -7,9 +7,6 @@
 # Post DietPi setup, pre-k3s
 sed -i '1s/$/cgroup_memory=1 cgroup_enable=memory/' /boot/cmdline.txt
 
-# Installing k3s worker per https://docs.k3s.io/quick-start
-curl -sfL https://get.k3s.io | K3S_URL="https://$2:6443" K3S_TOKEN=$1 sh -
-
 # running kubectl logs -n longhorn-system pod/longhorn-manager-d97hr -p
 # time="2024-11-15T10:18:02Z" level=fatal msg="Error starting manager: Failed environment check, please make sure you have iscsiadm/open-iscsi installed on the host: failed to execute: /usr/bin/nsenter [nsenter --mount=/host/proc/453509/ns/mnt --net=/host/proc/453509/ns/net iscsiadm --version], output , stderr nsenter: failed to execute iscsiadm: No such file or directory\n: exit status 127" func=main.main.DaemonCmd.func3 file="daemon.go:92"
 # 
@@ -27,3 +24,6 @@ sudo systemctl enable --now iscsid
 # looks like healthy after that, i didn't restart all machines. 
 # so let's make sure we do the above since it's a missing package 
 # from dietpi and it doesn't seem to have a place to install it
+
+# Installing k3s worker per https://docs.k3s.io/quick-start
+curl -sfL https://get.k3s.io | K3S_URL="https://$2:6443" K3S_TOKEN=$1 sh -
