1a30ac9945a9fb953a77073838f2b1e6064f8868
solar <45709370+mdsolarflare@users.noreply.github.com>
2025-01-23 19:16:42 -0800

    Create inprogress.sh

---
+++ scripts/inprogress.sh
@@ -0,0 +1,53 @@
+#NAME           STATUS   ROLES                         AGE     VERSION
+#dietpi.node4   Ready    worker                        205d    v1.29.5+k3s1
+#dietpi.node1   Ready    control-plane,master,worker   205d    v1.29.5+k3s1
+#dietpi.node2   Ready    worker                        205d    v1.29.5+k3s1
+#dietpi.node3   Ready    worker                        205d    v1.29.5+k3s1
+#xaviernx       Ready    worker                        9m52s   v1.31.4+k3s1
+
+# BASH SCRIPTS FTW :^) jk rethink labeling
+node_names=$(kubectl get nodes -o custom-columns=NAME:.metadata.name | awk '{print $1}' | tail -n +2)
+for node in $node_names; do
+  # Replace 'my-command' with your actual command
+  echo "kubectl label nodes <resource name from above> kubernetes.io/role=worker"
+  kubectl label nodes $node kubernetes.io/role=worker
+  # Add additional commands to operate on each node here (optional)
+done
+
+kubectl label nodes xaviernx kubernetes.io/role=worker
+kubectl label node xaviernx ml-stack=nvidia
+kubectl label nodes ubuntu kubernetes.io/role=worker
+kubectl label node ubuntu ml-stack=rockchip
+
+# Installing helm per https://helm.sh/docs/intro/install/
+# I have helm installed but haven't been using it and don't understand the use case. commenting out for now, same for arkade
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+# chmod 700 get_helm.sh
+# ./get_helm.sh
+
+# print version to make sure it installed ok?
+# helm version
+
+
+
+
+# install argoCD - https://argoproj.github.io/cd/
+kubectl create namespace argocd  
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# This IP is from defaultpool yaml for metallb - TODO parameterize!
+kubectl patch service argocd-server -n argocd --patch '{ "spec": { "type": "LoadBalancer", "loadBalancerIP": "192.168.0.122" } }' 
+
+#Install longhorn - https://docs.k3s.io/storage
+# kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.6.0/deploy/longhorn.yaml
+# Required packages that i believe to be missing for longhorn/k3s on dietpi
+sudo apt update
+sudo apt install -y open-iscsi
+sudo systemctl enable --now iscsid
+
+
+
+
+
+# my cool kid debugging area - TODO
+# kubectl get all -n argocd
+# kubectl exec -it <pod-name> -n <namespace> -- /bin/bash
