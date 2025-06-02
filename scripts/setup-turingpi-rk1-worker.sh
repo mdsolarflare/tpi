edb34b8aa5df1d37560a8b8a74b9f9c9408fa0a1
solar <45709370+mdsolarflare@users.noreply.github.com>
2025-06-01 18:35:19 -0700

    add iptables

--- scripts/setup-turingpi-rk1-worker.sh
+++ scripts/setup-turingpi-rk1-worker.sh
@@ -1,6 +1,7 @@
 # Assign the mac to a IP on the dhcp server
 sudo apt update
 sudo apt upgrade -y
+sudo apt install -y iptables
 
 # Installing k3s worker per https://docs.k3s.io/quick-start
 curl -sfL https://get.k3s.io | K3S_URL="https://$2:6443" K3S_TOKEN=$1 sh -
