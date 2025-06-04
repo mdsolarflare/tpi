01bb481841c55e1330db09d43431ebe6ccf9df6f
solar <45709370+mdsolarflare@users.noreply.github.com>
2025-06-04 01:04:17 -0700

    Update setup-dietpi-worker.sh

--- scripts/setup-dietpi-worker.sh
+++ scripts/setup-dietpi-worker.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # Usage
-# ./setup-worker.sh $token $masterIp
 
 # Post DietPi setup, pre-k3s
 sed -i '1s/$/cgroup_memory=1 cgroup_enable=memory/' /boot/cmdline.txt
 sudo apt install -y iptables
 
-# Installing k3s worker per https://docs.k3s.io/quick-start
-curl -sfL https://get.k3s.io | K3S_URL="https://$2:6443" K3S_TOKEN=$1 sh -
