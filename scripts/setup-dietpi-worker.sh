080fd82f7d9bee688bb77882b6f50fa0f0be9b7b
solar <45709370+mdsolarflare@users.noreply.github.com>
2025-06-04 19:20:31 -0700

    Update and rename setup-dietpi-worker.sh to setup-dietpi45.sh

--- scripts/setup-dietpi-worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Usage
-
-# Post DietPi setup, pre-k3s
-sed -i '1s/$/cgroup_memory=1 cgroup_enable=memory/' /boot/cmdline.txt
-sudo apt install -y iptables
-
---
+++ scripts/setup-dietpi45.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Usage
+
+# Post DietPi setup, pre-k3s
+# sed -i '1s/$/cgroup_memory=1 cgroup_enable=memory/' /boot/cmdline.txt
+# sudo apt install -y iptables
+
