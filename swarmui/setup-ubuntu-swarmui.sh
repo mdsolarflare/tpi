92df37d60eedeb3f9664bf4670942d836be50de7
solar <45709370+mdsolarflare@users.noreply.github.com>
2025-06-14 23:04:51 -0700

    Update setup-ubuntu-swarmui.sh

--- swarmui/setup-ubuntu-swarmui.sh
+++ swarmui/setup-ubuntu-swarmui.sh
@@ -19,7 +19,12 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo docker run hello-world
+# Restart the Docker service and add your user to the docker group, so that you don't need to use the command with sudo .
+sudo systemctl restart docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+docker run hello-world
 
 
 
