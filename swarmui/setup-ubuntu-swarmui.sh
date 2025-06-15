1be54deca46ad8a669d588179bbd4fdc5f5e3ea2
solar <45709370+mdsolarflare@users.noreply.github.com>
2025-06-14 23:16:38 -0700

    Update setup-ubuntu-swarmui.sh

--- swarmui/setup-ubuntu-swarmui.sh
+++ swarmui/setup-ubuntu-swarmui.sh
@@ -26,5 +26,24 @@ newgrp docker
 
 docker run hello-world
 
+# verify nvidia container toolkit installation
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
 
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
+sudo apt-get update
+NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
+sudo apt-get install -y nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
+
+# configure for docker
+sudo nvidia-ctk runtime configure --runtime=docker\
+sudo systemctl restart docker
+
+# verify git is installed
+sudo apt install -y git
+
+# Clone!
+git clone https://github.com/mcmonkeyprojects/SwarmUI
