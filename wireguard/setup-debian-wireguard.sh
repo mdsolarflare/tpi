2b5df14a7753e5286b9c1dfc0d7647d8f1021f63
solar <45709370+mdsolarflare@users.noreply.github.com>
2025-06-21 14:13:10 -0700

    Update setup-debian-wireguard.sh

--- wireguard/setup-debian-wireguard.sh
+++ wireguard/setup-debian-wireguard.sh
@@ -1,1 +1,6 @@
-# DietPi, install via software. done!
+# DietPi, install wireguard via software.
+# reference: https://www.wireguard.com/install/
+
+# For duckdns - https://www.duckdns.org/install.jsp
+# OR
+# It's in dietpi-config under network miscellaneous
