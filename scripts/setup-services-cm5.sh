cc9954b44b06decd33c0da0d95a9a7b16c18149c
solar <45709370+mdsolarflare@users.noreply.github.com>
2025-12-31 17:04:52 -0800

    Update setup-services-cm5.sh

--- scripts/setup-services-cm5.sh
+++ scripts/setup-services-cm5.sh
@@ -3,13 +3,6 @@
 # DietPi Debian - https://dietpi.com/#download
 # Debian        - https://www.debian.org/intro/why_debian
 
-# Install wireguard via dietpi-software
-# -- reference: https://www.wireguard.com/install/
-# Install openssh server via dietpi-software
-# Install docker via dietpi-software
-# Install duckdns via dietpi-config
-# -- For duckdns - https://www.duckdns.org/install.jsp OR It's in dietpi-config under network miscellaneous
-
 # Restart the Docker service and add your user to the docker group, so that you don't need to use the command with sudo .
 sudo systemctl restart docker
 sudo usermod -aG docker $USER
@@ -20,7 +13,7 @@ docker run hello-world
 # Install pihole - https://docs.pi-hole.net/docker/
 # See compose file neighbor
 # Run docker compose up -d to build and start Pi-hole (on older systems, the syntax here may be docker-compose up -d)
-wget https://raw.githubusercontent.com/mdsolarflare/tpi/refs/heads/main/docker/pihole/pihole-compose.yml
+wget https://raw.githubusercontent.com/mdsolarflare/tpi/refs/heads/main/docker/virtualized-services.yml
 # Don't forget to update your password :^) in the file
 docker compose up -d
 
